b4ae15b1d277c9909a9ba8bf24261910a54c1929
DemoJameson <demojameson@gmail.com>
2022-11-16 18:05:57 +0800

    feat: add "Skip Title Screen" option, enabled by default

---
+++ Cuphead.DebugMod/Components/SkipTitleScreen.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx.CupheadDebugMod.Config;
+
+namespace BepInEx.CupheadDebugMod.Components;
+
+public class SkipTitleScreen : PluginComponent {
+    private void Update() {
+        if (Settings.SkipTitleScreen.Value && CurrentSceneName == nameof(Scenes.scene_title)) {
+#if !v1_0
+            CreditsScreen.goodEnding = true;
+#endif
+            SettingsData.Data.hasBootedUpGame = true;
+            PlayerManager.SetPlayerCanJoin(PlayerId.PlayerOne, canJoin: true, promptBeforeJoin: false);
+            SceneLoader.LoadScene(Scenes.scene_slot_select, SceneLoader.Transition.Iris, SceneLoader.Transition.Fade, SceneLoader.Icon.None);
+        }
+    }
+}
