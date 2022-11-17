36a8bf552613a065fa2a2e4d821bda4e25491fff
DemoJameson <demojameson@gmail.com>
2022-11-17 17:13:08 +0800

    feat: better enable debug console

--- Cuphead.DebugMod/Components/EnableDebugConsole.cs
+++ Cuphead.DebugMod/Components/EnableDebugConsole.cs
@@ -1,14 +1,18 @@
 ﻿using HarmonyLib;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
 using UnityEngine;
 
-namespace BepInEx.CupheadDebugMod.Components; 
+namespace BepInEx.CupheadDebugMod.Components;
 
 [HarmonyPatch]
 public class EnableDebugConsole : PluginComponent {
-    [HarmonyPatch(typeof(Debug), nameof(Debug.isDebugBuild), MethodType.Getter)]
-    [HarmonyPrefix]
-    private static bool DebugIsDebugBuild(ref bool __result) {
-        __result = true;
-        return false;
+    [HarmonyPatch(typeof(DebugConsole), nameof(DebugConsole.Update))]
+    [HarmonyILManipulator]
+    private static void DebugConsoleUpdate(ILContext ilContext) {
+        ILCursor ilCursor = new(ilContext);
+        if (ilCursor.TryGotoNext(i => i.MatchCall<Debug>("get_isDebugBuild"))) {
+            ilCursor.Remove().Emit(OpCodes.Ldc_I4_1);
+        }
     }
 }
