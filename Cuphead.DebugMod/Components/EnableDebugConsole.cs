8fd90c79ca1523558c6fb20dcaba416f779bb888
DemoJameson <demojameson@gmail.com>
2022-11-17 14:41:43 +0800

    feat: enable debug console

---
+++ Cuphead.DebugMod/Components/EnableDebugConsole.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace BepInEx.CupheadDebugMod.Components; 
+
+[HarmonyPatch]
+public class EnableDebugConsole : PluginComponent {
+    [HarmonyPatch(typeof(Debug), nameof(Debug.isDebugBuild), MethodType.Getter)]
+    [HarmonyPrefix]
+    private static bool DebugIsDebugBuild(ref bool __result) {
+        __result = true;
+        return false;
+    }
+}
